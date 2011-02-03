87b4aaa517ae4d28ed20ce7251af0e891567f6f2
thiago.noronha@gmail.com <thiago.noronha@gmail.com@015e5c1f-a503-04f8-c584-ace6056cb9f1>
2011-02-03 13:46:18 +0000

    Enunciado da lista (no main.cc).

--- trunk/polinomios/src/main.cc
+++ trunk/polinomios/src/main.cc
@@ -1,3 +1,23 @@
+// Lista sobre Tipos Abstratos de Dados - TAD.
+//
+// Introdução: Um polinômio de grau n é uma função do tipo
+// P(x) = a0 + a1*x + a2*x2 + ... + an*xn, onde a0, a1, a2, ... , an pertencem
+// ao conjunto dos números reais. Qualquer polinômio de grau n pode ser
+// representado em um programa de computador por um vetor p com n + 1 posições,
+// onde cada posição p[i] do vetor armazena o valor do coeficiente
+// ai, i = {0, ... , n}. Por exemplo: o polinômio de grau 4
+// Q(x) = 5 + 3x2 + 2x4 = 5x0 + 0x1 + 3x2 + 0x3 + 2x4,
+// pode ser representado pelo vetor "float q[5] = {5, 0, 3, 0, 2};"
+//
+// Questão 1.
+// Implemente em polinomio.h e polinomio.cc um TAD Polinomio.
+//
+// Questão 2. Escreva um programa em main.cc que lê um polinomio P(x)
+// de um arquivo input.txt e indefinidamente lê um número x do teclado e exibe
+// na tela o valor de P(x). O primeiro número do arquivo n representa o grau do
+// polinômio e é seguido por n + 1 número reais que representam os
+// cooeficientes de P(x).
+
 #include "easytesting/polinomios/src/polinomios.h"
 
 #include <iostream>
@@ -5,10 +25,6 @@
 
 using namespace std;
 
-// Le um polinomio P(x) de uma arquivo e indefinidamente le um numero x
-// do teclado e exibe na tela o valor de P(x).
-// O primeiro nUmero do arquivo 'n' representa o grau do polinomio e
-// eh seguido por n + 1 numeros reais que representam os cooeficientes de P(x).
 int main() {
   ifstream fin("input.txt");
   int n;
@@ -21,6 +37,7 @@ int main() {
   delete [] v;
 
   while(true) {
+    cout << "Digite o valor de x: ";
     float x;
     cin >> x;
     cout << "P(" << x << ") = " << p.Avaliar(x) << endl;
--- trunk/polinomios/src/polinomios.h
+++ trunk/polinomios/src/polinomios.h
@@ -1,18 +1,4 @@
 // Copyright 2010 Thiago Ferreira de Noronha.
-//
-// Algoritmos e Estruturas de Dados II
-//
-// Lista sobre Tipos Abstratos de Dados - TAD.
-//
-// Introdução: Um polinômio de grau n é uma função do tipo
-// P(x) = a0 + a1*x + a2*x2 + ... + an*xn, onde a0, a1, a2, ... , an pertencem ao
-// conjunto dos números reais. Qualquer polinômio de grau n pode ser
-// representado em um programa de computador por um vetor p com n + 1 posições,
-// onde cada posição p[i] do vetor armazena o valor do coeficiente
-// ai, i  {0, ..., n}. Por exemplo: o polinômio de grau 4,
-// Q(x) = 5 + 3x2 + 2x4 = 5x0 + 0x1 + 3x2 + 0x3 + 2x4,
-// pode ser representado pelo vetor "float q[5] = {5, 0, 3, 0, 2};"
-
 
 #ifndef POLINOMIOS_H_
 
