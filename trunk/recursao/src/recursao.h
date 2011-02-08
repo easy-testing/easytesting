002c9855816b3a9896b2bed83cb20b0161b0f0e9
MatheusPacheco100@gmail.com <MatheusPacheco100@gmail.com@015e5c1f-a503-04f8-c584-ace6056cb9f1>
2011-02-08 18:09:58 +0000

    Adequação do código ao GSG

--- trunk/recursao/src/recursao.h
+++ trunk/recursao/src/recursao.h
@@ -1,9 +1,8 @@
-//TODO(matheus): Fazer um merge destes exercícios e os de iteração.
-//  Devem ser os mesmos nas duas listas.
+// Copyright 2010 Thiago Noronha
 
-#ifndef RECURSAO_H_
+#ifndef RECURSAO_SRC_RECURSAO_H_
 
-#define RECURSAO_H_
+#define RECURSAO_SRC_RECURSAO_H_
 
 // Retorna o valor do fatorial de n.
 int fat(int n);
@@ -11,29 +10,29 @@ int fat(int n);
 // Retorna k elevado a n.
 int pow(int k, int n);
 
-// Retorna o menor múltiplo comum de a e b.
+// Retorna o menor multiplo comum de a e b.
 int mmc(int a, int b);
 
-// Retorna o máximo divisor comum entre a e b.
+// Retorna o maximo divisor comum entre a e b.
 int mdc(int a, int b);
 
-// Retorna o máximo divisor comum entre a, b e c.
+// Retorna o maximo divisor comum entre a, b e c.
 int mdc3(int a, int b, int c);
 
-// Retorna o n-ésimo termo da série de Fibonacci.
+// Retorna o n-esimo termo da serie de Fibonacci.
 int fib(int n);
 
-// Retorna o resto da divisão de a por b.
+// Retorna o resto da divisao de a por b.
 int resto(int a, int b);
 
-// Retorna o resultado da divisão inteira de a por b.
+// Retorna o resultado da divisao inteira de a por b.
 int div_(int a, int b);
 
-// Retorna a soma dos dígitos de um inteiro positivo n.
+// Retorna a soma dos digitos de um inteiro positivo n.
 // Por exemplo: dig(123) retorna 6.
 int dig(int n);
 
-// Retorna a raiz quadrada de n (com precisão de 0.001).
+// Retorna a raiz quadrada de n (com precisao de 0.001).
 double sqrt_(double n);
 
-#endif  // RECURSAO_H_
+#endif  // RECURSAO_SRC_RECURSAO_H_
