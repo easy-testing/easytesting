821142f4441f485312ea7dec775ddef6ed004ac8
MatheusPacheco100@gmail.com <MatheusPacheco100@gmail.com@015e5c1f-a503-04f8-c584-ace6056cb9f1>
2011-02-10 21:18:19 +0000

    Teste da lista de Funções

--- trunk/funcoes/src/funcoes.cc
+++ trunk/funcoes/src/funcoes.cc
@@ -1,37 +1,38 @@
+// Copyright 2010 Matheus Pacheco
 #include<math.h>
 
 #define PI 3.1415
 
 float media(float a, float b, float c) {
-   return (a + b + c) / 3.0;
+  return (a + b + c) / 3.0;
 }
 
 float media_ponderada(float a, float b, float c) {
-   return (3 * a + 4 * b + 5 * c) / 12.0;
+  return (3 * a + 4 * b + 5 * c) / 12.0;
 }
 
 float perimetro(float r) {
-   return 2 * PI * r;
+  return 2 * PI * r;
 }
 
 float area_circulo(float r) {
-   return PI * r * r;
+  return PI * r * r;
 }
 
 float area_triangulo(float b, float c) {
-   return b * c / 2.0;
+  return b * c / 2.0;
 }
 
 float area_caixa(float a, float b, float c) {
-   return 2 * (a * b + a * c + b * c);
+  return 2 * (a * b + a * c + b * c);
 }
 
 float volume_caixa(float a, float b, float c) {
- return a * b * c;
+  return a * b * c;
 }
 
 float area_cilindro(float r, float h) {
-   return perimetro(r) * h + 2 * area_circulo(r);
+  return perimetro(r) * h + 2 * area_circulo(r);
 }
 
 float volume_cilindro(float r, float h) {
@@ -39,10 +40,10 @@ float volume_cilindro(float r, float h) {
 }
 
 float hipotenusa(float b, float c) {
-   return sqrt( b * b + c * c);
+  return sqrt( b * b + c * c);
 }
 
-float raiz_positiva(float a, float b,float c) {
-   float delta = b*b - 4*a*c;
-   return ((- b) + sqrt(delta)) / 2 * a;
+float raiz_positiva(float a, float b, float c) {
+  float delta = b*b - 4*a*c;
+  return ((- b) + sqrt(delta)) / 2 * a;
 }
--- trunk/funcoes/src/funcoes.h
+++ trunk/funcoes/src/funcoes.h
@@ -1,36 +1,41 @@
+// Copyright 2010 Thiago Noronha
+#ifndef FUNCOES_SRC_FUNCOES_H_
+#define FUNCOES_SRC_FUNCOES_H_
 #define PI 3.1415
 
-// Calcula a média de a, b e c.
+// Calcula a media de a, b e c.
 float media(float a, float b, float c);
 
-// Calcula a média ponderada de a, b e c onde a tem peso 3, b 4 e c 5.
+// Calcula a media ponderada de a, b e c onde a tem peso 3, b 4 e c 5.
 float media_ponderada(float a, float b, float c);
 
-// Calcula o perímetro de um círculo de raio r.
+// Calcula o perimetro de um circulo de raio r.
 float perimetro(float r);
 
-// Calcula a área de um círculo de raio r.
+// Calcula a area de um circulo de raio r.
 float area_circulo(float r);
 
-// Calcula a área de um triângulo de base b e altura c.
+// Calcula a area de um triangulo de base b e altura c.
 float area_triangulo(float b, float c);
 
-// Calcula a área de uma caixa de dimensões a, b e c.
+// Calcula a area de uma caixa de dimensoes a, b e c.
 float area_caixa(float a, float b, float c);
 
-// Calcula o volume de uma caixa de dimensões a, b e c.
-float volume_caixa(float a, float b,float c);
+// Calcula o volume de uma caixa de dimensoes a, b e c.
+float volume_caixa(float a, float b, float c);
 
-// Calcula a área de um cilindro de raio r e altura h.
+// Calcula a area de um cilindro de raio r e altura h.
 float area_cilindro(float r, float h);
 
 // Calcula o volume de um cilindro de raio r e altura h.
 float volume_cilindro(float r, float h);
 
-// Calcula a hipotenusa de um triângulo retângulo,
-// cujos lados conhecidos são b e c.
-float hipotenusa(float b,float c);
+// Calcula a hipotenusa de um triangulo retangulo,
+// cujos lados conhecidos sao b e c.
+float hipotenusa(float b, float c);
 
-// Calcula a raiz positiva da equação do segundo grau definida
+// Calcula a raiz positiva da equacao do segundo grau definida
 // por a.x2 + b.x + c.
-float raiz_positiva(float a, float b,float c);
+float raiz_positiva(float a, float b, float c);
+
+#endif  // FUNCOES_SRC_FUNCOES_H_
