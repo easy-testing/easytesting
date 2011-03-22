263fbb1f7d7a6f0ccecd94632260c3a9f683d188
thiago.noronha@gmail.com <thiago.noronha@gmail.com@015e5c1f-a503-04f8-c584-ace6056cb9f1>
2011-03-22 13:01:47 +0000

    Branch da lista de vetores para release.

---
+++ branches/release/vetores/src/main.cc
@@ -0,0 +1,31 @@
+// Copyright 2010 Universidade Federal de Minas Gerais (UFMG)
+//
+// Lista de exercício sobre estruturas de dados homogênias - Vetores.
+//
+// QUESTÃO 1:
+// Implemente em vetores.cc as funções descritas em vetores.h.
+//
+// QUESTÃO 2:
+// Escreva um programa em C++ que lê uma sequência de números de um arquivo
+// e armazena em um vetor. Assuma que a quantidade de numeros no arquivo
+// não ultrapassa o limite máximo de elementos do vetor (e.g. 1000).
+// Use este programa para testar as funções que você implementou na questão 1.
+
+#include <fstream>
+
+#include "vetores/src/vetores.h"
+
+// Lê uma sequência de números de um arquivo
+// e armazena em um vetor. Assuma que a quantidade de números no arquivo
+// não ultrapassa o limite máximo de elementos do vetor (e.g. 1000).
+int main() {
+  std::ifstream input;
+  float numero_arq;
+  int v[1000];
+  input.open("input.txt");
+  for (int i = 0 ; input >> numero_arq ; i++) {
+    v[i] = numero_arq;
+  }
+  input.close();
+  return 0;
+}
