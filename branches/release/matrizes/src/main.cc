1f47dc4386f437abfee2dbee5331cab0ddc58678
thiago.noronha@gmail.com <thiago.noronha@gmail.com@015e5c1f-a503-04f8-c584-ace6056cb9f1>
2011-03-22 13:30:38 +0000

    Branch inicial da Lista de Matrizes para preparar o release.

---
+++ branches/release/matrizes/src/main.cc
@@ -0,0 +1,37 @@
+// Copyright 2010 Universidade Federal de Minas Gerais (UFMG)
+//
+// Lista de exercícios sobre estruturas de dados homogenias - Matrizes.
+//
+// QUESTÃO 1:
+// Implemente em matrizes.cc as funções descritas em matrizes.h.
+//
+// QUESTÃO 2:
+// Escreva um programa que lê de um arquivo o número de linhas, o número de
+// colunas e todos os coeficientes de uma matriz e imprime esta matriz na tela.
+// Use este programa para testar se as funções que você implementou na questão 1
+// estão corretas.
+
+#include <fstream>
+
+#include "matrizes/src/matrizes.h"
+
+// Lê de um arquivo o número de linhas, o número de
+// colunas e todos os coeficientes de uma matriz
+// e imprime esta matriz na tela.
+int main(void) {
+  std::ifstream input;
+  float a[100][100], entrada_arq;
+  int n, m;
+  input.open("input.txt");
+  input >> n >> m;
+  for (int i = 0 ; i < n ; i++) {
+    for (int j = 0 ; j < m ; j++) {
+      input >> entrada_arq;
+      a[i][j] = entrada_arq;
+    }
+  }
+  MostraMatriz(n, m, a);
+  input.close();
+  return 0;
+}
+
---
+++ branches/release/matrizes/src/matrizes.h
@@ -0,0 +1,34 @@
+// Copyright 2010 Universidade Federal de Minas Gerais (UFMG)
+
+#ifndef MATRIZES_SRC_MATRIZES_H_
+#define MATRIZES_SRC_MATRIZES_H_
+
+// Recebe uma matriz quadrada de dimensão n e
+// retorna a média dos elementos armazenados na matriz.
+float MediaMatriz(int n, float mat[][100]);
+
+// Retorna através de A uma matriz identidade de dimensão n.
+void Identidade(int n, float A[][100]);
+
+// Recebe uma matriz quadrada A de dimensão n e
+// retorna uma matriz T que é a transposta de A.
+void Transposta(int n, float A[][100], float T[][100]);
+
+// Testa se uma matriz quadrada A de dimensão n é simétrica.
+bool Simetrica(int n, float A[][100]);
+
+// Recebe duas matrizes quadradas A e B de dimensão n
+// e retorna uma matriz S que é a soma de A e B.
+void SomaMatriz(int n, float A[][100], float B[][100], float S[][100]);
+
+// Recebe duas matrizes quadradas A e B de dimensão n
+// e retorna uma matriz P que é o produto de A e B.
+void MultMatriz(int n, float A[][100], float B[][100], float P[][100]);
+
+// Mostra os coeficientes de uma matriz na tela no formato:
+// | a[0][0] a[0][1] a[0][2] a[0][3] |
+// | a[1][0] a[1][1] a[1][2] a[1][3] |
+// | a[2][0] a[2][1] a[2][2] a[2][3] |
+void MostraMatriz(int n, int m, float a[][100]);
+
+#endif  // MATRIZES_SRC_MATRIZES_H_
