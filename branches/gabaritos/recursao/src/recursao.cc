5d7e1f7fd6031d45103341bf333db7100e67dd12
thiago.noronha@gmail.com <thiago.noronha@gmail.com@015e5c1f-a503-04f8-c584-ace6056cb9f1>
2011-03-25 00:36:08 +0000

    Gabarito de Recursão.

--- branches/gabaritos/recursao/src/recursao.cc
+++ branches/gabaritos/recursao/src/recursao.cc
@@ -4,45 +4,91 @@
 #include <math.h>
 
 int fat(int n) {
-  return 0;  // TODO.
+  if ((n == 1) or (n == 0))
+    return 1;
+  else
+    return n * fat(n-1);
 }
 
 int pow(int k, int n) {
-  return 0;  // TODO.
+  if (n == 0)
+    return 1;
+  else
+    return k * pow(k, n - 1);
 }
 
 int mdc(int a, int b) {
-  return 0;  // TODO.
+  if (a % b == 0)
+    return b;
+  else
+    return mdc(b, a%b);
 }
 
 int mdc3(int a, int b, int c) {
-  return 0;  // TODO.
+  return mdc(mdc(a, b), c);
 }
 
 int mmc(int a, int b) {
-  return 0;  // TODO.
+  return (a * b) / mdc(a, b);
 }
 
 int fib(int n) {
-  return 0;  // TODO.
+  if (n == 0)
+    return 0;
+  else if (n == 1)
+    return 1;
+  else
+    return fib(n - 1) + fib(n - 2);
 }
 
+
 int resto(int a, int b) {
-  return 0;  // TODO.
+  if (a < b)
+    return a;
+  else
+    return resto(a - b, b);
 }
 
 int div_(int a, int b) {
-  return 0;  // TODO.
+  if (a < b)
+    return 0;
+  else
+    return 1 + div_(a - b, b);
 }
 
 int dig(int n) {
-  return 0;  // TODO.
+  if (n < 10)
+    return n;
+  else
+    return n % 10 + dig(n / 10);
+}
+
+bool TemDivisorPrimoMenorQueK(int n, int k) {
+  if (k == 1)
+    return false;
+  else if (n % k == 0)
+    return true;
+  else
+    return TemDivisorPrimoMenorQueK(n, k - 1);
 }
 
 bool primo(int n) {
-  return false;  // TODO.
+  if (n <= 1)
+    return false;
+  else if (n == 2)
+    return true;
+  else
+    return !TemDivisorPrimoMenorQueK(n, n - 1);
+}
+
+double sqrt_aux(double n, double x) {
+  if (fabs(x * x - n) < 0.001) {
+    return x;
+  } else {
+    return  sqrt_aux(n, x - (x * x - n) / (2 * x));
+  }
 }
 
 double sqrt_(double n) {
-  return 0.0;  // TODO.
+  return sqrt_aux(n, n);
 }
