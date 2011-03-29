be43d05044ddd3950982a0b87451dc8b57729ceb
thiago.noronha@gmail.com <thiago.noronha@gmail.com@015e5c1f-a503-04f8-c584-ace6056cb9f1>
2011-03-29 21:40:26 +0000

    Lista de stack<Type> (sem testes)

---
+++ trunk/stack/src/main.cc
@@ -0,0 +1,25 @@
+// Copyright 2011 Universidade Federal de Minas Gerais (UFMG)
+
+#include <iostream>
+#include <fstream>
+#include "stack/src/stack.h"
+
+using namespace std;
+
+// TODO(tfn): Escrever enunciado.
+int main() {
+  // Lê os dados de um arquivo.
+  ifstream fin("input.txt");
+  stack<float> pilha;
+  float x;
+  while (fin >> x) {
+    pilha.push(x);
+  }
+
+  // Imprime-os em ordem inversa a do arquivo.
+  while (!pilha.empty()) {
+    cout << pilha.top() << endl;
+    pilha.pop();
+  }
+  return 0;
+}
---
+++ trunk/stack/src/stack.h
@@ -0,0 +1,46 @@
+// Copyright 2011 Universidade Federal de Minas Gerais (UFMG)
+
+#ifndef SET_SRC_SET_H_
+#define SET_SRC_SET_H_
+
+#include "list/src/list.h"
+
+// Implementa uma estrutura de dados Pilha.
+template<class Type>
+class stack {
+ public:
+  // Cria uma pilha vazia em O(1).
+  stack() {
+  }
+
+  // Testa se a pilha está vazia em O(1).
+  bool empty() {
+    return list_.empty();
+  }
+
+  // Retorna o número de elementos na pilha em O(1).
+  int size() {
+    return list_.size();
+  }
+
+  // Retorna o elemento que está no topo da pilha em O(1).
+  Type& top() {
+    return list_.front();
+  }
+
+  // Insere x no topo da pilha em O(1).
+  void push(Type x) {
+    list_.push_front(x);
+  }
+
+  // Remove o elemento que está no topo da pilha em O(1).
+  void pop() {
+    list_.pop_front();
+  }
+
+ private:
+  // Lista que representa a pilha.
+  list<Type> list_;
+};  // end class set.
+
+#endif  // SET_SRC_SET_H_
