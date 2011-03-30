65b4676f02a73451ee4ac0990dd5bf8edad9eaaa
thiago.noronha@gmail.com <thiago.noronha@gmail.com@015e5c1f-a503-04f8-c584-ace6056cb9f1>
2011-03-30 00:06:33 +0000

    Enunciado da lista de números complexos.

--- trunk/complexo/src/main.cc
+++ trunk/complexo/src/main.cc
@@ -1,4 +1,17 @@
 // Copyright 2010 Universidade Federal de Minas Gerais (UFMG)
+//
+// Lista de exercícios sobre Tipos Abstratos de Dados.
+// Aplicação: Números Complexos.
+//
+// QUESTÃO 1:
+// Implemente em complexo.cc as funções descritas em complexo.h.
+//
+// QUESTÃO 2:
+// Escreva um programa que simula uma calculadora de números complexos.
+// Ele deve apresentar um menu com as operações de soma, subtração,
+// multiplicação, divisão, módulo e conjugado. Após o usuário escolher uma das
+// funções, o programa deve ler o valor dos parâmetros e em seguida exibir o
+// resultado da função.
 
 #include <math.h>
 #include <iostream>
@@ -6,27 +19,7 @@
 
 using namespace std;
 
-void CalcularRaizes(float a, float b, float c, Complexo* r1, Complexo* r2) {
-  float delta = b * b - 4 * a * c;
-  if (delta >= 0.0) {
-    *r1 = Complexo((-b + sqrt(delta)) / 2 * a, 0.0);
-    *r2 = Complexo((-b - sqrt(delta)) / 2 * a, 0.0);
-  } else {
-    *r1 = Complexo(-b / 2 * a,   sqrt(-delta) / 2 * a);
-    *r2 = Complexo(-b / 2 * a, - sqrt(-delta) / 2 * a);
-  }
-}
-
-// Le os coeficientes de uma equacao de segundo grau do teclado e
-// imprime na tela as raizes da equacao.
 int main() {
-    cout << "Digite os coeficientes da equacao de segundo grau";
-    cout << " f(x) = a.x^2 + b.x + c" << endl;
-    float a, b, c;
-    cin >> a >> b >> c;
-    Complexo raiz1, raiz2;
-    CalcularRaizes(a, b, c, &raiz1, &raiz2);
-    cout << "x'  = " << raiz1.real() << " + " << raiz1.imag() << "i\n";
-    cout << "x'' = " << raiz2.real() << " + " << raiz2.imag() << "i\n";
-    return 0;
+  // TODO.
+  return 0;
 }
