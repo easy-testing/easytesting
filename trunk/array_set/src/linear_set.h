1aa2f0c2cd85bd5aeb74662d5401f7b0d5a6b00d
thiago.noronha@gmail.com <thiago.noronha@gmail.com@015e5c1f-a503-04f8-c584-ace6056cb9f1>
2011-03-31 15:17:44 +0000

    Base para a lista de conjuntos com array

---
+++ trunk/array_set/src/linear_set.h
@@ -0,0 +1,56 @@
+// Copyright 2011 Universidade Federal de Minas Gerais (UFMG)
+#ifndef SET_SRC_SET_H_
+#define SET_SRC_SET_H_
+
+#include "list/src/list.h"
+
+// Implementa um conjunto. O calculo da complexidade das funções assume que a
+// árvore esta sempre balanceada, o que não é garantido nesta implentação.
+// Ou seja, considera-se que a altura da arvore eh O(log n),
+// onde n eh o numero de elementos no conjunto.
+template<class Type>
+class set {
+ public:
+  // Cria um conjunto vazio em O(1).
+  set() {
+  }
+
+  // Testa se o cojunto esta vazio em O(1).
+  bool empty() {
+    return false;
+  }
+
+  // Retorna o número de elementos no cojunto em O(1).
+  int size() {
+    return 0;
+  }
+
+  // Testa se x pertece ao conjunto em O(log n).
+  bool find(Type x) {
+    return false;
+  }
+
+  // Insere x no conjunto em O(log n). Retorna false se x já estava no conjunto
+  // r true caso contrário.
+  bool insert(Type x) {
+    return false;
+  }
+
+  // Remove x do conjunto em O(log n). x deve pertencer ao conjunto.
+  void erase(Type x) {
+  }
+
+  // Insere todos os elementos do conjunto no final de l em O(n)
+  void ToList(list<Type>* l) {
+  }
+
+  // Remove todos os elementos do conjunto em O(n).
+  void clear() {
+  }
+
+ private:
+  // Lista que representa o conjunto.
+  list<Type> list_;
+};  // end class linear_set.
+
+#endif  // SET_SRC_SET_H_
