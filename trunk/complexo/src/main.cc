c013fa16a8fe4fa3dfd70ce4e8f28ddc0f0e4751
MatheusPacheco100@gmail.com <MatheusPacheco100@gmail.com@015e5c1f-a503-04f8-c584-ace6056cb9f1>
2011-04-13 00:11:37 +0000

    Implementação da Função Main()

--- trunk/complexo/src/main.cc
+++ trunk/complexo/src/main.cc
@@ -15,11 +15,84 @@
 
 #include <math.h>
 #include <iostream>
+#include <sstream>
 #include "complexo/src/complexo.h"
 
 using namespace std;
 
+string ToString(Complexo& z) {
+    stringstream output;
+    output << z.real();
+    if (z.imag() >= 0.0) {
+      output << " + " << z.imag() << "i";
+    } else {
+      output << " - " << -z.imag() << "i";
+    }
+    return output.str();
+}
+
+int Menu() {
+  const int num_op = 7;
+  int op;
+
+  cout << "                     Universidade Federal de Minas Gerais\n\n";
+  cout << "\n\n                              MENU PRINCIPAL\n";
+  cout << "                     Calculadora de Numeros Complexos\n\n\n";
+  cout << "                              1.  Somar\n";
+  cout << "                              2.  Subtrair\n";
+  cout << "                              3.  Multiplicar\n";
+  cout << "                              4.  Dividir\n";
+  cout << "                              5.  Modulo\n";
+  cout << "                              6.  Conjugado\n";
+  cout << "                              7.  Sair\n\n\n";
+  cout << "     Digite o numero da opcao desejada: ";
+  cin >> op;
+  while (!(op >= 1 && op <= num_op)) {
+    cout << "     Opcao Invalida! Digite o numero da opcao desejada: ";
+    cin >> op;
+  }
+  return op;
+}
+
+Complexo Leitura() {
+  float real;
+  float imag;
+
+  cout << "Digite o valor da parte real do numero complexo: ";
+  cin >> real;
+  cout << "Digite o valor da parte imaginaria do numero complexo: ";
+  cin >> imag;
+  Complexo c(real, imag);
+  return c;
+}
+
+void Resposta(Complexo c) {
+  cout << "\nResposta : " << ToString(c) << "\n\n\n";
+}
+
+void Resposta(double x) {
+  cout << "\nResposta : " << x << "\n\n\n";
+}
+
+void Operacao(int op) {
+  Complexo c1;
+  Complexo c2;
+  c1 = Leitura();
+  switch (op) {
+    case 1: c2 = Leitura(); Resposta(c1 + c2); break;
+    case 2: c2 = Leitura(); Resposta(c1 - c2); break;
+    case 3: c2 = Leitura(); Resposta(c1 * c2); break;
+    case 4: c2 = Leitura(); Resposta(c1 / c2); break;
+    case 5: Resposta(c1.modulo()); break;
+    case 6: Resposta(c1.conjugado()); break;
+  }
+}
+
 int main() {
-  // TODO.
+  int op = Menu();
+  while (op != 7) {
+    Operacao(op);
+    op = Menu();
+  }
   return 0;
 }
