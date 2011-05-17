40a6252af1b0419fcf324ed4bce6d44fc75f88d1
thiago.noronha@gmail.com <thiago.noronha@gmail.com@015e5c1f-a503-04f8-c584-ace6056cb9f1>
2011-05-17 02:12:12 +0000

    Release da lista de TAD/Complexos euclidianos

---
+++ branches/release/complexo/src/complexo.cc
@@ -0,0 +1,65 @@
+// Copyright 2010 Universidade Federal de Minas Gerais (UFMG)
+
+#include "complexo/src/complexo.h"
+
+#include <math.h>
+
+Complexo::Complexo() {
+  // TODO.
+}
+
+Complexo::Complexo(double a) {
+  // TODO.
+}
+
+Complexo::Complexo(double a, double b) {
+  // TODO.
+}
+
+double Complexo::real() {
+  return 0.0;  // TODO.
+}
+
+double Complexo::imag() {
+  return 0.0;  // TODO.
+}
+
+bool Complexo::operator==(Complexo x) {
+  return false;  // TODO.
+}
+
+void Complexo::operator=(Complexo x) {
+  // TODO.
+}
+
+double Complexo::modulo() {
+  return 0.0;  // TODO.
+}
+
+Complexo Complexo::conjugado() {
+  return *this;  // TODO.
+}
+
+Complexo Complexo::simetrico() {
+  return *this;  // TODO.
+}
+
+Complexo Complexo::inverso() {
+  return *this;  // TODO.
+}
+
+Complexo Complexo::operator+(Complexo y) {
+  return *this;  // TODO.
+}
+
+Complexo Complexo::operator-(Complexo y) {
+  return *this;  // TODO.
+}
+
+Complexo Complexo::operator*(Complexo y) {
+  return *this;  // TODO.
+}
+
+Complexo Complexo::operator/(Complexo y) {
+  return *this;  // TODO.
+}
