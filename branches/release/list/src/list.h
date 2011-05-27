a5eb1ebcd84d5b2df26ee45f1c97767af8055382
thiago.noronha@gmail.com <thiago.noronha@gmail.com@015e5c1f-a503-04f8-c584-ace6056cb9f1>
2011-05-27 18:24:05 +0000

    Versão de Release de list<Type>.

---
+++ branches/release/list/src/list.h
@@ -0,0 +1,124 @@
+// Copyright 2011 Universidade Federal de Minas Gerais (UFMG)
+
+#ifndef LIST_H_
+#define LIST_H_
+
+#include <stdlib.h>
+#include "list/src/node.h"
+
+template<class Type>
+class list {
+ public:
+  // Cria uma lista vazia em O(1).
+  list() {
+    end_ = new Node<Type>();
+  }
+
+  // Cria a lista com os mesmo elementos de l em O(n).
+  list(list<Type>& l) {
+    end_ = new Node<Type>();
+    clear();
+    merge(l);
+  }
+
+  // Remove todos os elementos da lista para liberar a memoria.
+  ~list() {
+    clear();
+    delete end_;
+  }
+
+  // Retorna um iterador para o primeiro elemento da lista em O(1).
+  Node<Type>* begin() {
+    return end_;  // TODO.
+  }
+
+  // Retorna um iterador para "depois" do ultimo elemento da lista em O(1).
+  Node<Type>* end() {
+    return end_;  // TODO.
+  }
+
+   // Testa se a lista esta vazia em O(1).
+  bool empty() {
+    return false;  // TODO.
+  }
+
+  // Retorna o valor do primeiro elemento da lista em O(1).
+  // PRECONDIÇÃO: a lista não pode estar vazia.
+  Type front() {
+    Type a;
+    return a;  // TODO.
+  }
+
+  // Retorna o valor do último elemento da lista em O(1).
+  // PRECONDIÇÃO: a lista não pode estar vazia.
+  Type back() {
+    Type a;
+    return a;  // TODO.
+  }
+
+  // Retorna o número de elementos da lista em O(n).
+  int size() {
+    return 0;  // TODO.
+  }
+
+  // Insere x no início da lista em O(1).
+  void push_front(Type x) {
+    // TODO.
+  }
+
+  // Remove o primeiro elemento da lista em O(1).
+  void pop_front() {
+    // TODO.
+  }
+
+  // Insere x no final da lista em O(1).
+  void push_back(Type x) {
+    // TODO.
+  }
+
+  // Remove o último elemento da lista em O(1).
+  void pop_back() {
+    // TODO.
+  }
+
+  // Retona um ponteiro para o primeiro nó da lista que contém x.
+  // Se x não está na lista, retorna um ponteiro para end().
+  Node<Type>* find(Type x) {
+    return end_;  // TODO.
+  }
+
+  // Insere x antes do elemento identificado por posision em O(1) e retorna
+  // um ponteiro para o nó que foi criado para armazenar x.
+  Node<Type>* insert(Node<Type>* node, Type x) {
+    return end_;  // TODO.
+  }
+
+  // Apaga o elemento indicado por posision em O(1).
+  void erase(Node<Type>* node) {
+    // TODO.
+  }
+
+
+  // Remove todos os elementos da lista corrente em O(n).
+  void clear() {
+    // TODO.
+  }
+
+  // Concatena os elementos de l no final da lista corrente em O(n).
+  void merge(list<Type>& l) {
+    // TODO.
+  }
+
+  // Faz a lista corrente ficar igual a l em O(n);
+  void operator=(list<Type>& l) {
+    // TODO.
+  }
+
+private:
+  // Ponteiro para o nó sentinela da lista.
+  Node<Type>* end_;
+
+  friend class Teste;
+};
+
+#endif  // LIST_H_
---
+++ branches/release/list/src/main.cc
@@ -0,0 +1,65 @@
+// Copyright 2011 Universidade Federal de Minas Gerais (UFMG)
+//
+// Lista sobre Listas Encadeadas.
+//
+// Questão 1.
+// Implemente o TAD list<Type> em list.h.
+//
+// Questão 2.
+// Escreva uam função "void Print(list<float>& l)" que recebe uma lista como
+// parâmetro e impreme na tela os elementos da lista no formato
+// "[a1, a2, ..., an]".
+//
+// Questão 3.
+// Escreva uma função "Node<float>* MinList(list<float>& l))" que recebe uma
+// lista l como parâmetro e retorna um ponteiro para o nó que contém o menor
+// elemento de l.
+//
+// Questão 4.
+// Escreva uma função "void Sort(list<float>* l)" que recebe uma lista l como
+// parâmetro e ordena os elementos de l do menor para o maior.
+//
+// Questão 5.
+// Escreva um programa que (i) lê uma quantidade indeterminada de números de um
+// arquivo, (ii) armazena estes números em uma lista, (iii) ordena os elementos
+// da lista e (iv) imprime na tela os números ordenados.
+// Nota: no arquivo não existe nenhuma indicação de quantos números estão
+// contidos nele.
+
+#include "list/src/list.h"
+
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Impreme na tela os elementos da lista l no formato "[a1, a2, ..., an]".
+void Print(list<float>& l) {
+  // TODO.
+}
+
+// Retorna um ponteiro para o nó que contém o menor elemento de l.
+Node<float>* MinList(list<float>& l) {
+  return NULL;  // TODO.
+}
+
+// Ordena os elementos de l do menor para o maior.
+void Sort(list<float>* l) {
+  // TODO.
+}
+
+// Lê uma quantidade indeterminada de números de um arquivo e imprime na tela os
+// números ordenados.
+int main() {
+  ifstream fin("input.txt");
+  float x;
+  list<float> l;
+  while (fin >> x) {
+    l.push_back(x);
+  }
+  Print(l);
+  Sort(&l);
+  Print(l);
+  return 0;
+}
