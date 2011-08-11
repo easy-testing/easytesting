2dc23065caf8d5b4b7c2d93d60fafc56ac5de182
thiago.noronha@gmail.com <thiago.noronha@gmail.com@015e5c1f-a503-04f8-c584-ace6056cb9f1>
2011-08-11 19:30:54 +0000

    Apaga o diretorio Release

--- branches/release/vetores/test/vetores_test.cc
+++
@@ -1,9 +0,0 @@
-// copyright 2010 Universidade Federal de Minas Gerais (UFMG)
-
-#include "vetores/test/vetores_test.h"
-
-int main(int argc, char** argv) {
-    testing::InitGoogleTest(&argc, argv);
-    testing::GTEST_FLAG(print_time) = false;
-    return RUN_ALL_TESTS();
-}
