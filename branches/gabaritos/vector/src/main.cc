875a8b76fd90eac0c75fd2c52871af87173b8adb
thiago.noronha@gmail.com <thiago.noronha@gmail.com@015e5c1f-a503-04f8-c584-ace6056cb9f1>
2011-08-12 17:38:26 +0000

    Esqueleto da lista de vector<Type>

---
+++ branches/gabaritos/vector/src/main.cc
@@ -0,0 +1,15 @@
+// Copyright 2011 Universidade Federal de Minas Gerais (UFMG)
+//
+// Lista sobre vetores dinâmicos.
+//
+// Questão 1.
+// Implemente o TAD vector<Type> em vector.h utilizando.
+
+#include "vector/src/vector.h"
+
+using namespace std;
+
+
+int main() {
+  // TODO.
+}
