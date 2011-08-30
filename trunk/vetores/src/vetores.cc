27152068ab35ead30726a1db8847afd6c6297afe
willmont7@gmail.com <willmont7@gmail.com@015e5c1f-a503-04f8-c584-ace6056cb9f1>
2011-08-30 05:05:55 +0000

    Implementação das funções.

--- trunk/vetores/src/vetores.cc
+++ trunk/vetores/src/vetores.cc
@@ -1,29 +1,58 @@
 // Copyright 2010 Universidade Federal de Minas Gerais (UFMG)
 
 #include "vetores/src/vetores.h"
+#include <iostream>
 
 using namespace std;
 
 float Media(float v[], int n) {
-  return 0.0;  //TODO.
+  float ret = 0.;
+  for(int i = 0;i < n;i++)
+    ret += v[i];
+  ret /= (float)n;
+  return ret;
 }
 
 float Variancia(float v[], int n) {
-  return 0.0;  //TODO.
+  float ret = 0.;
+  float media;
+  for(int i = 0;i < n;i++)
+    ret += v[i]*v[i];
+  ret /= (float)n;
+  media = Media(v, n);
+  media *= media;
+  ret -= media;
+  return ret;
 }
 
 float Maior(float v[], int n) {
-  return 0.0;  //TODO.
+  int higher = 0;
+  for(int i = 1;i < n;i++)
+    if(v[i] > v[higher])
+      higher = i;
+  return v[higher];
 }
 
 float Menor(float v[], int n) {
-  return 0.0;  //TODO.
+  int lower = 0;
+  for(int i = 1;i < n;i++)
+    if(v[i] < v[lower])
+      lower = i;
+    return v[lower];
 }
 
 float ProdutoEscalar(float u[], float v[], int n) {
-  return 0.0;  //TODO.
+  float ret = 0.;
+  for(int i = 0;i < n;i++)
+    ret += v[i]*u[i];
+  return ret;
 }
 
 void MostraVetor(float v[], int n) {
-  //TODO.
+  if(n < 1)
+    return;
+  cout << "{" << v[0];
+  for(int i = 1;i < n;i++)
+    cout << ", " << v[i];
+  cout << "}" << endl;
 }
