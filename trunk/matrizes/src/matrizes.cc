62e37f15cb54b869714c271c2658d6d92fd9628e
willmont7@gmail.com <willmont7@gmail.com@015e5c1f-a503-04f8-c584-ace6056cb9f1>
2011-09-09 14:49:15 +0000

    Exercícios adicionais

--- trunk/matrizes/src/matrizes.cc
+++ trunk/matrizes/src/matrizes.cc
@@ -1,11 +1,17 @@
 // Copyright 2010 Universidade Federal de Minas Gerais (UFMG)
 
-#include "matrizes/src/matrizes.h"
+#include "matrizes.h"
 
 #include <iostream>
-#include <stdio.h>
 
 using namespace std;
+
+/* Compara dois tipos float | true->iguais | falso->diferentes */
+bool cmpfloat(float a, float b){
+  if (a - b > 0.000001 || a - b < -0.000001)
+    return false;
+  return true;
+}
 
 float MediaMatriz(int n, float mat[][MAX]) {
   float ret = 0.;
@@ -35,7 +41,7 @@ void Transposta(int n, float A[][MAX], float T[][MAX]) {
 bool Simetrica(int n, float A[][MAX]) {
   for(int i = 0;i < n;++i)
     for(int j = i+1;j < n;++j)
-      if(A[i][j] != A[j][i])
+      if(!cmpfloat(A[i][j], A[j][i]))
         return false;
   return true;
 }
@@ -60,9 +66,49 @@ void MultMatriz(int n, float A[][MAX], float B[][MAX], float P[][MAX]) {
 
 void MostraMatriz(int n, int m, float a[][MAX]) {
   for(int i = 0;i < n;++i){
-    printf("| ");
+    cout << "| ";
     for(int j = 0;j < m;++j)
       cout << a[i][j] << " ";
-    printf("|\n");
+    cout << "|\n";
   }
+}
+
+//////////////////////////////////////////
+//         EXERCICIOS ADICIONAIS        //
+//////////////////////////////////////////
+
+void EscalarMatriz(int n, int m, int escalar, float A[][MAX], float Out[][MAX]){
+  for(int i = 0;i < n;++i)
+    for(int j = 0;j < m;++j)
+      Out[i][j] = A[i][j] * escalar;
+}
+
+float DeterminanteMatriz(float A[][MAX]){
+  float pos, neg;
+  float det = 0;
+  for(int i = 0;i < 3;++i){
+    pos = neg = 1;
+    for(int j = 0;j < 3;++j){
+      pos *= A[ j ] [ (i+j)%3 ];      // Multiplica os elementos da parte positiva do det.
+      neg *= A[ j ] [ 2 - (i+j)%3 ];  // Multiplica os elementos da parte negativa do det.
+    }
+    det += pos - neg;
+  }
+  return det;
+}
+
+bool VerificaTriangularInferior(int n, float A[][MAX]){
+  for(int i = 0;i < n;++i)
+    for(int j = i+1;j < n;++j)
+      if(!cmpfloat(A[i][j], 0.)) // Verifica se algum elemento da parte diagonal superior nao eh nula
+        return false;
+  return true;
+}
+
+bool VerificaTriangularSuperior(int n, float A[][MAX]){
+  for(int i = 0;i < n;++i)
+    for(int j = 0;j < i;++j)
+      if(!cmpfloat(A[i][j], 0)) // Verifica se algum elemento da parte diagonal inferior nao eh nula
+        return false;
+      return true;
 }
