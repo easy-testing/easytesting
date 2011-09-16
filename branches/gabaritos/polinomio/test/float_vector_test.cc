e07fb434031ea7f9fa21cf19a2cd4727333f7ad0
thiago.noronha@gmail.com <thiago.noronha@gmail.com@015e5c1f-a503-04f8-c584-ace6056cb9f1>
2011-09-16 01:22:58 +0000

    FloatVector tests

--- branches/gabaritos/polinomio/test/float_vector_test.cc
+++ branches/gabaritos/polinomio/test/float_vector_test.cc
@@ -1,6 +1,6 @@
-// Copyright 2010 Universidade Federal de Minas Gerais (UFMG)
+// Copyright 2011 Universidade Federal de Minas Gerais (UFMG)
 
-#include "polinomio/test/polinomio_test.h"
+#include "float_vector/test/float_vector_test.h"
 
 int main(int argc, char** argv) {
     testing::InitGoogleTest(&argc, argv);
