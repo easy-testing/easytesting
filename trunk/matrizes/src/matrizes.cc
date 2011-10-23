61d9f05a63b788fbfee9621fcffa73556d2d8eb8
willmont7@gmail.com <willmont7@gmail.com@015e5c1f-a503-04f8-c584-ace6056cb9f1>
2011-10-23 02:00:33 +0000

    Rollback: "Release da lista de matrizes".

--- trunk/matrizes/src/matrizes.cc
+++ trunk/matrizes/src/matrizes.cc
@@ -3,116 +3,34 @@
 #include "matrizes/src/matrizes.h"
 
 #include <iostream>
+#include <stdio.h>
 
-using std::cout;
-
-/* Compara dois tipos float | true->iguais | falso->diferentes */
-bool cmpfloat(float a, float b) {
-  if (a - b > 0.000001 || a - b < -0.000001)
-    return false;
-  return true;
-}
+using namespace std;
 
 float MediaMatriz(int n, float mat[][MAX]) {
-  float ret = 0.;
-  for (int i = 0;i < n;++i)
-    for (int j = 0;j < n;++j)
-      ret += mat[i][j];
-
-  ret /= static_cast<double>(n*n);
-
-  return ret/n*n;
+  return 0.0;  // TODO.
 }
 
 void Identidade(int n, float A[][MAX]) {
-  for (int i = 0;i < n;++i) {
-    for (int j = 0;j < n;++j)
-      A[i][j] = 0;
-    A[i][i] = 1;
-  }
+  // TODO.
 }
 
 void Transposta(int n, float A[][MAX], float T[][MAX]) {
-  for (int i = 0;i < n;++i)
-    for (int j = 0;j < n;++j)
-      T[j][i] = A[i][j];
+  // TODO.
 }
 
 bool Simetrica(int n, float A[][MAX]) {
-  for (int i = 0;i < n;++i)
-    for (int j = i+1;j < n;++j)
-      if (!cmpfloat(A[i][j], A[j][i]))
-        return false;
-  return true;
+  return false;  // TODO.
 }
 
 void SomaMatriz(int n, float A[][MAX], float B[][MAX], float S[][MAX]) {
-  for (int i = 0;i < n;++i)
-    for (int j = 0;j < n;++j)
-      S[i][j] = A[i][j] + B[i][j];
+  // TODO.
 }
 
 void MultMatriz(int n, float A[][MAX], float B[][MAX], float P[][MAX]) {
-  float t;
-  for (int i = 0;i < n;++i)
-    for (int j = 0;j < n;++j) {
-      t = 0.;
-      for (int k = 0;k < n;++k) {
-        t += A[i][k]*B[k][j];
-      }
-      P[i][j] = t;
-    }
+  // TODO.
 }
 
 void MostraMatriz(int n, int m, float a[][MAX]) {
-  for (int i = 0;i < n;++i) {
-    cout << "| ";
-    for (int j = 0;j < m;++j)
-      cout << a[i][j] << " ";
-    cout << "|\n";
-  }
-}
-
-//////////////////////////////////////////
-//         EXERCICIOS ADICIONAIS        //
-//////////////////////////////////////////
-
-void EscalarMatriz(int n, int m, int k, float A[][MAX], float Out[][MAX]) {
-  for (int i = 0;i < n;++i)
-    for (int j = 0;j < m;++j)
-      Out[i][j] = A[i][j] * k;
-}
-
-float DeterminanteMatriz(float A[][MAX]) {
-  float pos, neg;
-  float det = 0;
-  for (int i = 0;i < 3;++i) {
-    pos = neg = 1;
-    for (int j = 0;j < 3;++j) {
-      // Multiplica os elementos da parte positiva do det.
-      pos *= A[ j ] [ (i+j)%3 ];
-      // Multiplica os elementos da parte negativa do det.
-      neg *= A[ j ] [ 2 - (i+j)%3 ];
-    }
-    det += pos - neg;
-  }
-  return det;
-}
-
-bool VerificaTriangularInferior(int n, float A[][MAX]) {
-  for (int i = 0;i < n;++i)
-    for (int j = i+1;j < n;++j)
-      // Verifica se algum elemento da parte diagonal superior nao eh nula.
-      if (!cmpfloat(A[i][j], 0.))
-        return false;
-  return true;
-}
-
-bool VerificaTriangularSuperior(int n, float A[][MAX]) {
-  for (int i = 0;i < n;++i)
-    for (int j = 0;j < i;++j)
-      // Verifica se algum elemento da parte diagonal inferior nao eh nula.
-      if (!cmpfloat(A[i][j], 0))
-        return false;
-  return true;
+  // TODO.
 }
