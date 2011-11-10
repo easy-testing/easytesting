9781b77930d2abb66386a74089a750ea70426da6
galmeida28@gmail.com <galmeida28@gmail.com@015e5c1f-a503-04f8-c584-ace6056cb9f1>
2011-11-10 16:45:44 +0000

    copiando vector para minha pasta

---
+++ branches/galmeida/vector/test/vector_test.h
@@ -0,0 +1,18 @@
+// Copyright 2011 Universidade Federal de Minas Gerais (UFMG)
+
+#ifndef BRANCHES_GABARITOS_VECTOR_TEST_VECTOR_TEST_H_
+#define BRANCHES_GABARITOS_VECTOR_TEST_VECTOR_TEST_H_
+
+#include "vector/src/vector.h"
+
+#include "gtest/gtest.h"
+
+// Classe base dos testes.
+class Teste : public testing::Test {
+};
+
+TEST_F(Teste, Nao_testa_nada_por_enquanto) {
+}
+
+#endif  // BRANCHES_GABARITOS_VECTOR_TEST_VECTOR_TEST_H_
+
