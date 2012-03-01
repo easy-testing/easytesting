bb60e1bc345292a0c93d4ba2fd69ac9c20df7e1b
thiago.noronha@gmail.com <thiago.noronha@gmail.com@015e5c1f-a503-04f8-c584-ace6056cb9f1>
2012-03-01 19:35:24 +0000

    apaga funcoes/ de Gabriel

--- branches/galmeida/funcoes2/test/funcoes_test.cc
+++
@@ -1,9 +0,0 @@
-// copyright 2010 Universidade Federal de Minas Gerais (UFMG)
-
-#include "funcoes2/test/funcoes_test.h"
-
-int main(int argc, char** argv) {
-    testing::InitGoogleTest(&argc, argv);
-    testing::GTEST_FLAG(print_time) = false;
-    return RUN_ALL_TESTS();
-}
--- branches/galmeida/funcoes3/test/funcoes_test.cc
+++
@@ -1,9 +0,0 @@
-// copyright 2010 Universidade Federal de Minas Gerais (UFMG)
-
-#include "funcoes3/test/funcoes_test.h"
-
-int main(int argc, char** argv) {
-    testing::InitGoogleTest(&argc, argv);
-    testing::GTEST_FLAG(print_time) = false;
-    return RUN_ALL_TESTS();
-}
