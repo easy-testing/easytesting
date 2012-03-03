8bc5bf39e28b38d7f257102a6ded13fc46d45057
thiago.noronha@gmail.com <thiago.noronha@gmail.com@015e5c1f-a503-04f8-c584-ace6056cb9f1>
2012-03-03 16:44:46 +0000

    Versão incompleta da lista polinomios

--- trunk/polinomio/src/main1.cc
+++ trunk/polinomio/src/main1.cc
@@ -1,14 +1,14 @@
 // Copyright 2010 Universidade Federal de Minas Gerais (UFMG)
 //
-// Lista sobre Tipos Abstratos de Dados - TAD: Polinômios.
+// Lista sobre vetores - polinômios.
 //
 // Introdução: Um polinômio de grau n é uma função do tipo
 // P(x) = a0 + a1*x + a2*x^2 + ... + an*x^n, onde a0, a1, a2, ... , an pertencem
 // ao conjunto dos números reais. Qualquer polinômio de grau n pode ser
-// representado em um programa de computador por um vetor p com n + 1 posições,
-// onde cada posição p[i] do vetor armazena o valor do coeficiente
-// ai, i = {0, ... , n}. Por exemplo: o polinômio de grau 4
-// Q(x) = 5 + 3*x^2 + 2*x^4 = 5x^0 + 0*x^1 + 3*x^2 + 0*x^3 + 2*x^4,
+// representado em um programa de computador por um vetor v com n + 1 posições,
+// onde cada posição v[i] do vetor armazena o valor do coeficiente
+// v[i], i = {0, ... , n}. Por exemplo: o polinômio de grau 4
+// Q(x) = 5 + 3*x^2 + 2*x^4 = 5*x^0 + 0*x^1 + 3*x^2 + 0*x^3 + 2*x^4,
 // pode ser representado pelo vetor "float q[5] = {5, 0, 3, 0, 2};"
 //
 // A derivada de um polinômio P(x) = a0 + a1*x + a2*x^2 + ... + an*x^n
@@ -23,68 +23,40 @@
 // Por exemplo: integral[Q(x)] = 5*x + 3/3*x^3 + 2/5*x^5 = 5*x + x^3 + 0.4*x^5.
 //
 // Questão 1.
-// Implemente em polinomio.h e polinomio.cc um TAD Polinomio.
+// Implemente em polinomio.cc as funções de polinomio.h.
 //
 // Questão 2.
 // Escreva um programa que lê um polinômio P(x) de um arquivo e indefinidamente
-// lê um número x do teclado e exibe na tela o valor de P(x).
+// lê um número x do teclado e exibe na tela o valor de P(x). Você pode assumir
+// que o grau máximo do polinômio é 99.
 //
 // Questão 3.
-// Escreva um programa que lê de um arquivo um polinômio P(x) e um intervalo
-// [a, b] e exibe a área da figura definida por P(x) no intervalo[a, b].
+// Vide arquivo main2.cc
 
 #include <fstream>
 #include <iostream>
 
 #include "polinomio/src/polinomio.h"
 
+#define MAX 100
+
 using namespace std;
 
 // Gabarito da Questão 2.
-int main2() {
-  ifstream fin("input.txt");
+int main() {
+  ifstream fin("input1.txt");
   int g;
   fin >> g;
-  Polinomio p(g);
+  float v[MAX];
   for (int i = 0; i <= g; i++) {
-    fin >> p.at(i);
+    fin >> v[i];
   }
+  Imprimir(g, v);
   while (true) {
     cout << "Digite o valor de x: ";
     float x;
     cin >> x;
-    cout << "P(" << x << ") = " << p.Avaliar(x) << endl;
+    cout << "P(" << x << ") = " << Avaliar(g, v, x) << endl;
   }
   return 0;
-}
-
-// Gabarito da Questão 3.
-float AreaPolinomio(Polinomio& p, float a, float b) {
-  Polinomio integral;
-  integral.Integrar(p);
-  return integral.Avaliar(b) - integral.Avaliar(a);
-}
-
-int main3() {
-  ifstream fin("input.txt");
-  int grau;
-  fin >> grau;
-  Polinomio p(grau);
-  for (int i = 0; i <= grau; i++) {
-    fin >> p.at(i);
-  }
-  float a;
-  fin >> a;
-  float b;
-  fin >> b;
-  cout << "Area = " << AreaPolinomio(p, a, b) << endl;
-  return 0;
-}
-
-/* Escolhe qual das questões executar.
-**************************************/
-
-int main() {
-  // return main2();
-  return main3();
 }
