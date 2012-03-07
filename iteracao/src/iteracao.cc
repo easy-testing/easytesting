8893ccf09e5adf05f9e7f868e88dc08338b47ebf
thiago.noronha@gmail.com <thiago.noronha@gmail.com@015e5c1f-a503-04f8-c584-ace6056cb9f1>
2012-03-07 22:01:39 +0000

    Tags iteracao/

--- iteracao/src/iteracao.cc
+++ iteracao/src/iteracao.cc
@@ -2,102 +2,46 @@
 
 #include "iteracao/src/iteracao.h"
 
-#include <cmath>
-
 int fat(int n) {
-  int f = 1;
-  while (n > 1) {
-    f = f * n;
-    n = n - 1;
-  }
-  return f;
+  return 0;  // TODO.
 }
 
 int pow(int k, int n) {
-  int p = 1;
-  while (n > 0) {
-    p *= k;
-    n--;
-  }
-  return p;
+  return 0;  // TODO.
 }
 
 int mdc(int a, int b) {
-  int r = a % b;
-  while (r != 0) {
-    a = b;
-    b = r;
-    r = a % b;
-  }
-  return b;
+  return 0;
 }
 
 int mdc3(int a, int b, int c) {
-  return mdc(mdc(a, b), c);
+  return 0;  // TODO.
 }
 
 int mmc(int a, int b) {
-  return (a * b) / mdc(a, b);
+  return 0;  // TODO.
 }
 
 int fib(int n) {
-  if (n <= 1) {
-    return n;
-  } else {
-      int a = 0;
-      int b = 1;
-      int c;
-      for (int i = 2; i <= n ; i++) {
-         c = a + b;
-         a = b;
-         b = c;
-      }
-      return c;
-  }
+  return 0;  // TODO.
 }
 
 int resto(int a, int b) {
-  while (a >= b) {
-    a = a - b;
-  }
-  return a;
+  return 0;  // TODO.
 }
 
 int div_(int a, int b) {
-  int soma = 0;
-  while (a >= b) {
-    a = a - b;
-    soma++;
-  }
-  return soma;
+  return 0;  // TODO.
 }
 
 int dig(int n) {
-  int soma = 0;
-  while (n > 0) {
-    soma = soma + n % 10;
-    n = n / 10;
-  }
-  return soma;
+  return 0;  // TODO.
 }
 
 bool primo(int n) {
-  if (n < 2) {
-    return false;
-  } else {
-      for (int i = 2; i < n; i++) {
-         if (n % i == 0) {
-            return false;
-         }
-      }
-      return true;
-  }
+  return false;  // TODO.
 }
 
 double sqrt_(double n) {
-  double x = n;
-  while (fabs(x * x - n) >= 0.001) {
-    x = x - (x * x - n) / (2 * x);
-  }
-  return x;
+  return 0;  // TODO.
 }
