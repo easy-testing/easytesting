7c8a894ec6edc1eb13b9c355f73ce82e7b2cac34
thiago.noronha@gmail.com <thiago.noronha@gmail.com@015e5c1f-a503-04f8-c584-ace6056cb9f1>
2012-03-07 21:54:04 +0000

    Tags complexo_euclidiano/

--- complexo_euclidiano/src/main.cc
+++ complexo_euclidiano/src/main.cc
@@ -24,85 +24,8 @@
 // segundo grau e imprime na tela as raízes (possivelmente complexas) desta
 // equação.
 
-#include <cmath>
-#include <iostream>
-#include <sstream>
-
 #include "complexo_euclidiano/src/complexo.h"
 
-using namespace std;
-
-void Imprimir(Complexo z) {
-  cout << z.real();
-  if (z.imag() >= 0.0) {
-    cout << " + " << z.imag() << "i";
-  } else {
-    cout << " - " << -z.imag() << "i";
-  }
-  cout << endl;
-}
-
-int LerOpcaoDoTeclado() {
-  int op;
-  cout << "1.  Somar\n";
-  cout << "2.  Subtrair\n";
-  cout << "3.  Multiplicar\n";
-  cout << "4.  Dividir\n";
-  cout << "5.  Modulo\n";
-  cout << "6.  Conjugado\n";
-  cout << "7.  Sair\n\n\n";
-  cout << "Digite o numero da opcao desejada: ";
-  cin >> op;
-  while (!(op >= 1 && op <= 7)) {
-    cout << "Opcao Invalida! Digite o numero da opcao desejada: ";
-    cin >> op;
-  }
-  return op;
-}
-
-Complexo LerComplexoDoTeclado() {
-  float real;
-  float imag;
-  cout << "Digite o valor da parte real do numero complexo: ";
-  cin >> real;
-  cout << "Digite o valor da parte imaginaria do numero complexo: ";
-  cin >> imag;
-  Complexo c(real, imag);
-  return c;
-}
-
-void ExecutarOperacao(int op) {
-  Complexo c1;
-  Complexo c2;
-  c1 = LerComplexoDoTeclado();
-  switch (op) {
-    case 1:
-      c2 = LerComplexoDoTeclado();
-      Imprimir(c1 + c2);
-      break;
-    case 2:
-      c2 = LerComplexoDoTeclado();
-      Imprimir(c1 - c2);
-      break;
-    case 3:
-      c2 = LerComplexoDoTeclado();
-      Imprimir(c1 * c2);
-      break;
-    case 4:
-      c2 = LerComplexoDoTeclado();
-      Imprimir(c1 / c2);
-      break;
-    case 5:
-      cout << c1.modulo() << endl;
-      break;
-    case 6:
-      Imprimir(c1.conjugado());
-      break;
-  }
-}
-
 int main() {
-  int op = LerOpcaoDoTeclado();
-  ExecutarOperacao(op);
-  return 0;
+  return 0;  // TODO.
 }
--- complexo_euclidiano/test/complexo_test.cc
+++ complexo_euclidiano/test/complexo_test.cc
@@ -5,5 +5,6 @@
 int main(int argc, char** argv) {
     testing::InitGoogleTest(&argc, argv);
     testing::GTEST_FLAG(print_time) = false;
+    testing::GTEST_FLAG(break_on_failure) = true;
     return RUN_ALL_TESTS();
 }
