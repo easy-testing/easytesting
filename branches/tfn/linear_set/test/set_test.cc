427c989271ecf27632b328ce0161b2e75ca8b9f7
thiago.noronha@gmail.com <thiago.noronha@gmail.com@015e5c1f-a503-04f8-c584-ace6056cb9f1>
2012-03-10 01:53:38 +0000

    Apaga listas que não serão mais usadas

--- branches/tfn/linear_set/test/set_test.cc
+++
@@ -1,9 +0,0 @@
-// Copyright 2011 Universidade Federal de Minas Gerais (UFMG)
-
-#include "linear_set/test/set_test.h"
-
-int main(int argc, char** argv) {
-    testing::InitGoogleTest(&argc, argv);
-    testing::GTEST_FLAG(print_time) = false;
-    return RUN_ALL_TESTS();
-}
