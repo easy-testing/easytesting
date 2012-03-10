f88feb7bb444095e8363e7a3a6a622c9e8f69300
thiago.noronha@gmail.com <thiago.noronha@gmail.com@015e5c1f-a503-04f8-c584-ace6056cb9f1>
2012-03-10 01:54:52 +0000

    Apaga listas que não serão mais usadas

--- branches/willmont7/template/template_list/test/list_test.cc
+++
@@ -1,9 +0,0 @@
-// Copyright 2011 Universidade Federal de Minas Gerais (UFMG)
-
-#include "list/test/list_test.h"
-
-int main(int argc, char** argv) {
-    testing::InitGoogleTest(&argc, argv);
-    testing::GTEST_FLAG(print_time) = false;
-    return RUN_ALL_TESTS();
-}
