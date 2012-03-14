be3f93ce5d5a039d2d6212d6155a63cff8c562a9
thiago.noronha@gmail.com <thiago.noronha@gmail.com@015e5c1f-a503-04f8-c584-ace6056cb9f1>
2012-03-14 17:03:19 +0000

    Esqueleto da lista de condicionais

---
+++ branches/vinicius/condicionais/src/condicionais.cc
@@ -0,0 +1,6 @@
+// Copyright 2010 Universidade Federal de Minas Gerais (UFMG)
+
+#include "condicionais/src/condicionais.h"
+
+#include <cmath>
+
---
+++ branches/vinicius/condicionais/src/condicionais.h
@@ -0,0 +1,8 @@
+// Copyright 2012 Universidade Federal de Minas Gerais (UFMG)
+
+#ifndef TRUNK_FUNCOES_SRC_FUNCOES_H_
+#define TRUNK_FUNCOES_SRC_FUNCOES_H_
+
+
+
+#endif  // TRUNK_FUNCOES_SRC_FUNCOES_H_
