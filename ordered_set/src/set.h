50bfc57425e231c1a56deca5b839d8fe1bf16757
thiago.noronha@gmail.com <thiago.noronha@gmail.com@015e5c1f-a503-04f8-c584-ace6056cb9f1>
2012-09-08 17:32:04 +0000

    Refactoring em lista de ordered_set

--- ordered_set/src/set.h
+++ ordered_set/src/set.h
@@ -14,37 +14,14 @@ struct Node;
 // NOTA: O cálculo da complexidade das funções assume que a árvore está
 // balanceada, ou seja, considera-se que a altura da arvore é O(log n), onde n
 // é a cardinalidade do conjunto. Entretanto, isto não é garantido nesta
-// implentação. Para simplificar as funções de complexidade,
-// denota-se n = size_.
+// implentação.
+// O tipo dos elementos contidos no conjunto é definido por SType.
+// O valor de SType deve ser definido em tempo de compilação.
+// Para simplificar as funções de complexidade, denota-se n = size_.
 class set {
  public:
   // Cria um conjunto vazio em O(1).
   set();
-
-  // Cria um conjunto com os mesmos elementos de s.
-  set(set& s);
-
-  // Libera toda a memória alocada para o conjunto.
-  ~set();
-
-  // Retorna um ponteiro para o MENOR elemento do conjunto em O(log n).
-  // Caso o conjunto esteja vazio, rentorna um ponteiro para set::end().
-  Node* begin();
-
-  // Retorna um ponteiro para o "marcador de fim" do conjunto em O(1).
-  // Nota: end() não é um elemento do conjunto.
-  Node* end();
-
-  // Retorna o elemento seguinte ao indicado por x no conjunto em O(log n).
-  // Precondição: x aponta para um dos elementos do conjunto.
-  Node* next(Node* x);
-
-  // Retorna o elemento anterior ao indicado por x no conjunto em O(log n).
-  // Precondição: x aponta para um dos elementos do conjunto.
-  Node* prev(Node* x);
-
-  // Retorna o valor do elemento indicado por x em O(1).
-  SType operator[](Node* x);
 
   // Testa se o cojunto está vazio em O(1).
   bool empty();
@@ -52,26 +29,49 @@ class set {
   // Retorna o número de elementos no conjunto em O(1).
   int size();
 
-  // Retorna um ponteiro para o elemento k em O(log n),
+  // Retorna um ponteiro para o primeiro elemento do conjunto em O(1).
+  // Caso o conjunto esteja vazio, rentorna um ponteiro para set::end().
+  Node* begin();
+
+  // Retorna um ponteiro para o "marcador de fim" do conjunto em O(1).
+  Node* end();
+
+  // Retorna o elemento seguinte ao indicado por x no conjunto em O(1).
+  // Se x aponta para o último elemento do conjunto, retorna set::end();
+  // Precondição: x aponta para um dos elementos do conjunto.
+  Node* next(Node* x);
+
+  // Retorna o elemento anterior ao indicado por x no conjunto em O(1).
+  // Se x aponta para o primeiro elemento do conjunto, retorna set::end();
+  // Precondição: x aponta para um dos elementos do cojunto, ou para set::end().
+  Node* prev(Node* x);
+
+  // Retorna o valor do elemento indicado por x em (1).
+  SType operator[](Node* x);
+
+  // Retorna um ponteiro para o elemento k em O(n),
   // ou um ponteiro para set::end() caso k não pertença ao conjunto.
   // OBS: Note que esta função NÃO retorna bool. Para testar se um elemento 'a'
   // pertence a um conjunto 'c', você deve escrever "if (c.find(a) != c.end())".
   Node* find(SType k);
 
-  // Insere k no conjunto em O(log n) e retorna um ponteiro para este elemento.
-  // Caso k já pertença ao conjunto, um novo elemento NÃO é inserido no conjunto
-  // e a função retorna um ponteiro o para o elemento k já existente.
+  // Insere k no conjunto em O(n).
+  // Caso k já pertença ao conjunto, um novo elemento NÃO é inserido no
+  // conjunto.
   void insert(SType k);
 
-  // Remove k do conjunto (caso lá ele esteja) em O(log n).
+  // Remove k do conjunto (caso lá ele esteja) em O(n).
   void erase(SType k);
 
   // Remove todos os elementos do conjunto.
   void clear();
 
   // Faz com que o conjunto corrente contenha exatamente os mesmos elementos
-  // do cojunto s.
+  // do cojunto s em O(n + m), onde m = s.size().
   void operator=(set& s);
+
+  // Libera toda a memória alocada para o conjunto em O(n).
+  ~set();
 
  private:
   // Número de elementos no conjunto.
