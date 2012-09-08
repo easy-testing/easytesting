1c119e9a8aa10b1782ec6f8948e1ae7b72ca4148
thiago.noronha@gmail.com <thiago.noronha@gmail.com@015e5c1f-a503-04f8-c584-ace6056cb9f1>
2012-09-08 19:23:57 +0000

    Lista de TAD hash_set

--- hash_set/src/set.cc
+++ hash_set/src/set.cc
@@ -1,5 +1,7 @@
 // Copyright 2011 Universidade Federal de Minas Gerais (UFMG)
 
+#include <unordered_set>
+
 #include "hash_set/src/set.h"
 #include "list/src/list.h"
 
@@ -11,12 +13,14 @@ struct Node {
 };
 
 set::set() {
-  size_ = 0;
+  size_ = 0;  // Inicialmente, o conjunto não tem elementos.
+  capacity_ = 1024; // Inicia com uma tabela com 2^10 linhas.
+  table_ = new list[capacity_];
 }
 
 // Retorna o primeiro elemento da primeira lista não vazia.
 Node* set::begin() {
-  for (int i = 0; i < kCapacity_; i++) {
+  for (int i = 0; i < capacity_; i++) {
     if (!table_[i].empty()) {
       return table_[i].begin();
     }
@@ -27,18 +31,18 @@ Node* set::begin() {
 // Retorna o "marcador de fim" do conjunto, ou seja,
 // O sentinela da última lista da tabela.
 Node* set::end() {
-  return table_[kCapacity_ - 1].end();
+  return table_[capacity_ - 1].end();
 }
 
 // Se x não é o último elemento da lista que o contém, retorna o elemento
 // seguinte a x nesta lista. Caso contrário, retorna o primeiro elemento da
 // próxima lista não vazia da tabela.
 Node* set::next(Node* x) {
-  int j = hash(x->key, kCapacity_);
+  int j = hash(x->key);
   if (x->next != table_[j].end()) {
     return x->next;
   } else {
-    for (int i = j + 1; i < kCapacity_; i++) {
+    for (int i = j + 1; i < capacity_; i++) {
       if (!table_[i].empty()) {
         return table_[i].begin();
       }
@@ -51,7 +55,7 @@ Node* set::next(Node* x) {
 // anterior a x nesta lista. Caso contrário, retorna o último elemento da
 // primeira lista não vazia anterior a lista de x .
 Node* set::prev(Node* x) {
-  int j = hash(x->key, kCapacity_);
+  int j = hash(x->key);
   if (x != table_[j].begin()) {
     return x->prev;
   } else {
@@ -78,7 +82,7 @@ int set::size() {
 
 Node* set::find(SType k) {
   // Procura pelo elemento k na lista onde k pode estar.
-  int j = hash(k, kCapacity_);
+  int j = hash(k);
   Node* i;
   for (i = table_[j].begin(); i != table_[j].end(); i = table_[j].next(i)) {
     if (i->key == k) {
@@ -89,9 +93,12 @@ Node* set::find(SType k) {
 }
 
 void set::insert(SType k) {
+  if (size_ == capacity_) {
+    rehash(capacity_ * 2);
+  }
   Node* x = find(k);
   if (x == end()) {
-    int j = hash(k, kCapacity_);
+    int j = hash(k);
     table_[j].insert(table_[j].begin(), k);
     size_++;
   }
@@ -100,25 +107,49 @@ void set::insert(SType k) {
 void set::erase(SType k) {
   Node* x = find(k);
   if (x != end()) {
-    int j = hash(k, kCapacity_);
+    int j = hash(k);
     table_[j].erase(x);
     size_--;
   }
 }
 
 void set::clear() {
-  for (int i = 0; i < kCapacity_; i++) {
+  for (int i = 0; i < capacity_; i++) {
     table_[i].clear();
   }
   size_ = 0;
 }
 
 void set::operator=(set& s) {
-  for (int i = 0; i < kCapacity_; i++) {
+  for (int i = 0; i < capacity_; i++) {
     table_[i] = s.table_[i];
   }
   size_ = s.size_;
 }
 
 set::~set() {
+  delete [] table_;
+}
+
+int set::hash(SType k) {
+  std::unordered_set<SType>::hasher fh;
+  return fh(k) % capacity_;
+}
+
+void set::rehash(int m) {
+  // Armazena a tabela atual.
+  list* old_table = table_;
+  int old_capacity = capacity_;
+  // Cria uma nova tabela vazia com 'm' linhas.
+  table_ = new list[m];
+  capacity_ = m;
+  // Insere todos os elementos na nova tabela.
+  for (int i = 0; i < old_capacity; i++) {
+    for (Node* j = old_table[i].begin();
+         j != old_table[i].end();
+         j = old_table[i].next(j)) {
+      insert(old_table[i][j]);
+    }
+  }
+  delete [] old_table;
 }
