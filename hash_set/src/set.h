7bb3b990e7a91e1dcf03e4a30e82028bb7723c25
thiago.noronha@gmail.com <thiago.noronha@gmail.com@015e5c1f-a503-04f8-c584-ace6056cb9f1>
2012-09-08 17:31:25 +0000

    Refactoring em lista de ordered_set

--- hash_set/src/set.h
+++ hash_set/src/set.h
@@ -16,25 +16,27 @@
 // Implementa um conjunto utilizando tabela hash.
 // NOTA: O cálculo da complexidade das funções assume que a função de hash
 // utilizada é uniformemente simples e que capacity_ > size_. Entretanto,
-// isto não é garantido nesta implentação. Para simplificar as funções de
-// complexidade, denota-se n = size_ e m = capacity_.
+// isto não é garantido nesta implentação.
+// O tipo dos elementos contidos no conjunto é definido por SType.
+// O valor de SType deve ser definido em tempo de compilação.
+// Para simplificar as funções de complexidade,
+// denota-se n = size_ e m = capacity_.
 class set {
  public:
   // Cria um conjunto vazio em O(1).
   set();
 
-  // Cria um conjunto com os mesmos elementos de s em O(n + m).
-  set(set& s);
+  // Testa se o cojunto está vazio em O(1).
+  bool empty();
 
-  // Libera toda a memória alocada para o conjunto em O(n + m).
-  ~set();
+  // Retorna o número de elementos no conjunto em O(1).
+  int size();
 
-  // Retorna um ponteiro para o PRIMEIRO elemento do conjunto em O(m).
+  // Retorna um ponteiro para o primeiro elemento do conjunto em O(m).
   // Caso o conjunto esteja vazio, rentorna um ponteiro para set::end().
   Node* begin();
 
   // Retorna um ponteiro para o "marcador de fim" do conjunto em O(1).
-  //
   Node* end();
 
   // Retorna o elemento seguinte ao indicado por x no conjunto em O(m).
@@ -44,17 +46,11 @@ class set {
 
   // Retorna o elemento anterior ao indicado por x no conjunto em O(m).
   // Se x aponta para o primeiro elemento do conjunto, retorna set::end();
-  // Precondição: x aponta para um dos elementos da lista.
+  // Precondição: x aponta para um dos elementos do cojunto, ou para set::end().
   Node* prev(Node* x);
 
   // Retorna o valor do elemento indicado por x em (1).
   SType operator[](Node* x);
-
-  // Testa se o cojunto está vazio em O(1).
-  bool empty();
-
-  // Retorna o número de elementos no conjunto em O(1).
-  int size();
 
   // Retorna um ponteiro para o elemento k em O(1),
   // ou um ponteiro para set::end() caso k não pertença ao conjunto.
@@ -62,9 +58,9 @@ class set {
   // pertence a um conjunto 'c', você deve escrever "if (c.find(a) != c.end())".
   Node* find(SType k);
 
-  // Insere k no conjunto em O(1) e retorna um ponteiro para este elemento.
-  // Caso k já pertença ao conjunto, um novo elemento NÃO é inserido no conjunto
-  // e a função retorna um ponteiro o para o elemento k já existente.
+  // Insere k no conjunto em O(1).
+  // Caso k já pertença ao conjunto, um novo elemento NÃO é inserido no
+  // conjunto.
   void insert(SType k);
 
   // Remove k do conjunto (caso lá ele esteja) em O(1).
@@ -74,15 +70,18 @@ class set {
   void clear();
 
   // Faz com que o conjunto corrente contenha exatamente os mesmos elementos
-  // do cojunto s.
+  // do cojunto s em O(n + m).
   void operator=(set& s);
+
+  // Libera toda a memória alocada para o conjunto em O(n).
+  ~set();
 
  private:
   // Número de elementos no cojunto.
   int size_;
 
   // Número de listas no vetor table_.
-  static const int kCapacity_ = 10000;
+  static const int kCapacity_ = 1000;
 
   // Vetor de listas que guarda os elementos do cojunto.
   list table_[kCapacity_];
