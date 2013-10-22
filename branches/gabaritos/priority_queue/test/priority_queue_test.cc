b650b3b7940edae43d4b18c76f84b5924b5372e2
thiago.noronha@gmail.com <thiago.noronha@gmail.com@015e5c1f-a503-04f8-c584-ace6056cb9f1>
2013-10-22 19:39:50 +0000

    Apaga branches antigos

--- branches/gabaritos/priority_queue/test/priority_queue_test.cc
+++
@@ -1,9 +0,0 @@
-// Copyright 2011 Universidade Federal de Minas Gerais (UFMG)
-
-#include "priority_queue/test/priority_queue_test.h"
-
-int main(int argc, char** argv) {
-    testing::InitGoogleTest(&argc, argv);
-    testing::GTEST_FLAG(print_time) = false;
-    return RUN_ALL_TESTS();
-}
