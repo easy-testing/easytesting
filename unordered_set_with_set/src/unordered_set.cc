2329127df31c3932b0e335e96f2e4276e9633147
thiagonoronha <thiagonoronha@015e5c1f-a503-04f8-c584-ace6056cb9f1>
2013-10-29 02:48:08 +0000

    Refactoring de unordered_set_with_set

--- unordered_set_with_set/src/unordered_set.cc
+++ unordered_set_with_set/src/unordered_set.cc
@@ -1,7 +1,5 @@
 // Copyright 2011 Universidade Federal de Minas Gerais (UFMG)
 
-#include <unordered_set>
-
 #include "unordered_set_with_set/src/unordered_set.h"
 #include "ordered_set_with_linked_list/src/set.h"
 
@@ -11,6 +9,10 @@ struct Node {
   Node* prev;  // Ponteiro para o nó anterior.
   Node* next;  // Ponteiro para o próximo nó.
 };
+
+int hash(SType k, int m) {
+  return m - 1;
+}
 
 unordered_set::unordered_set() {
   size_ = 0;  // Inicialmente, o conjunto não tem elementos.
@@ -38,7 +40,7 @@ Node* unordered_set::end() {
 // seguinte a x neste subconjunto. Caso contrário,
 // retorna o primeiro elemento do próximo subconjunto não vazio da tabela.
 Node* unordered_set::next(Node* x) {
-  int j = hash(x->key);
+  int j = hash(x->key, capacity_);
   if (x->next != table_[j].end()) {
     return x->next;
   } else {
@@ -56,7 +58,7 @@ Node* unordered_set::next(Node* x) {
 // retorna o último elemento do primeiro subconjunto não vazio anterior
 // ao subconjunto de x .
 Node* unordered_set::prev(Node* x) {
-  int j = hash(x->key);
+  int j = hash(x->key, capacity_);
   if (x != table_[j].begin()) {
     return x->prev;
   } else {
@@ -69,7 +71,7 @@ Node* unordered_set::prev(Node* x) {
   }
 }
 
-SType unordered_set::operator[](Node* x) {
+SType unordered_set::key(Node* x) {
   return x->key;
 }
 
@@ -82,7 +84,7 @@ int unordered_set::size() {
 }
 
 Node* unordered_set::find(SType k) {
-  int j = hash(k);
+  int j = hash(k, capacity_);
   Node* x = table_[j].find(k);
   if (x != table_[j].end()) {
     return x;
@@ -95,7 +97,7 @@ void unordered_set::insert(SType k) {
   if (size_ == capacity_) {
     rehash(capacity_ * 2);
   }
-  int j = hash(k);
+  int j = hash(k, capacity_);
   Node* x = table_[j].find(k);
   if (x == table_[j].end()) {
     table_[j].insert(k);
@@ -104,7 +106,7 @@ void unordered_set::insert(SType k) {
 }
 
 void unordered_set::erase(SType k) {
-  int j = hash(k);
+  int j = hash(k, capacity_);
   Node* x = table_[j].find(k);
   if (x != table_[j].end()) {
     table_[j].erase(k);
@@ -130,11 +132,6 @@ unordered_set::~unordered_set() {
   delete [] table_;
 }
 
-int unordered_set::hash(SType k) {
-  std::unordered_set<SType>::hasher fh;
-  return fh(k) % capacity_;
-}
-
 void unordered_set::rehash(int m) {
   // Armazena a tabela atual.
   set* old_table = table_;
@@ -147,7 +144,7 @@ void unordered_set::rehash(int m) {
     for (Node* j = old_table[i].begin();
          j != old_table[i].end();
          j = old_table[i].next(j)) {
-      insert(old_table[i][j]);
+      insert(old_table[i].key(j));
     }
   }
   delete [] old_table;
