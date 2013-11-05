0361f5e882304b1da2ed9f667802dd109db3af17
thiago.noronha@gmail.com <thiago.noronha@gmail.com@015e5c1f-a503-04f8-c584-ace6056cb9f1>
2013-11-05 23:59:25 +0000

    Tag da lista de unordered_set

--- tags/2013.2/ordered_set_with_linked_list/src/set.cc
+++ tags/2013.2/ordered_set_with_linked_list/src/set.cc
@@ -10,57 +10,97 @@ struct Node {
 };
 
 set::set() {
-  // TODO.
+  size_ = 0;
+  end_ = new Node;
+  end_->next = end_;
+  end_->prev = end_;
 }
 
 bool set::empty() {
-  return false;  // TODO.
+  return size_ == 0;
 }
 
 int set::size() {
-  return 0;  // TODO.
+  return size_;
 }
 
 Node* set::begin() {
-  return NULL;  // TODO.
+  return end_->next;
 }
 
 Node* set::end() {
-  return NULL;  // TODO.
+  return end_;
 }
 
 Node* set::next(Node* x) {
-  return NULL;  // TODO.
+  return x->next;
 }
 
 Node* set::prev(Node* x) {
-  return NULL;  // TODO.
+  return x->prev;
 }
 
-SType set::operator[](Node* x) {
-  return SType();  // TODO.
+SType set::key(Node* x) {
+  return x->key;
 }
 
 Node* set::find(SType k) {
-  return NULL;  // TODO.
+  for (Node* i = begin(); i != end(); i = next(i)) {
+    if (i->key == k) {
+      return i;
+    } else if (i->key > k) {
+      return end();
+    }
+  }
+  return end();  // k é maior que todos os elementos contidos no conjunto.
 }
 
 void set::insert(SType k) {
-  // TODO.
+  // Encontra o primeiro elemento que não é menor que k na lista encadeada.
+  Node* x = begin();
+  while (x != end() && x->key < k) {
+    x = next(x);
+  }
+  // Caso k já não esteja no conjunto, cria um novo nó para armazenar k.
+  if (x == end() || x->key != k) {
+    // Cria um novo nó e define o valor dos seus campos.
+    Node* node = new Node;
+    node->key = k;
+    node->prev = x->prev;
+    node->next = x;
+    // Ajusta o valor dos ponteiros dos nós adjacentes ao novo nó.
+    x->prev->next = node;
+    x->prev = node;
+    size_++;
+  }
 }
 
 void set::erase(SType k) {
-  // TODO.
+  Node* x = find(k);
+  if (x != end()) {
+    x->prev->next = x->next;
+    x->next->prev = x->prev;
+    delete x;
+    size_--;
+  }
 }
 
 void set::clear() {
-  // TODO.
+  while (!empty()) {
+    erase(begin()->key);
+  }
 }
 
 void set::operator=(set& s) {
-  // TODO.
+  clear();
+  if (!s.empty()) {
+    for (Node* i = s.end_->prev; i != s.end_; i = i->prev) {
+      insert(i->key);
+    }
+  }
 }
 
 set::~set() {
-  // TODO.
+  clear();
+  delete end_;
 }
