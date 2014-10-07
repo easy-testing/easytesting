d5ef50eea5cbe4fb1e504fd69771fc60ddc64949
thiago.noronha@gmail.com <thiago.noronha@gmail.com@015e5c1f-a503-04f8-c584-ace6056cb9f1>
2014-10-07 17:09:05 +0000

    Preparacao para a tag 2014.2

--- condicionais/test/condicionais_test.cc
+++
@@ -1,9 +0,0 @@
-// copyright 2010 Universidade Federal de Minas Gerais (UFMG)
-
-#include "condicionais/test/condicionais_test.h"
-
-int main(int argc, char** argv) {
-    testing::InitGoogleTest(&argc, argv);
-    testing::GTEST_FLAG(print_time) = false;
-    return RUN_ALL_TESTS();
-}
