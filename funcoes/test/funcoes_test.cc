8cfd9442042a53b6278aa0d076cc9f95dd92e9fd
thiago.noronha@gmail.com <thiago.noronha@gmail.com@015e5c1f-a503-04f8-c584-ace6056cb9f1>
2014-10-08 13:18:17 +0000

    Refactoring de funcoes

--- funcoes/test/funcoes_test.cc
+++ funcoes/test/funcoes_test.cc
@@ -1,6 +1,6 @@
-// copyright 2010 Universidade Federal de Minas Gerais (UFMG)
+// copyright 2014 Universidade Federal de Minas Gerais (UFMG)
 
-#include "funcoes/test/funcoes_test.h"
+#include "test/funcoes_test.h"
 
 int main(int argc, char** argv) {
     testing::InitGoogleTest(&argc, argv);
